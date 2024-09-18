e245b95fc11f0c6cbef71f35768d36da5bc758e5
Dave <josedavid0531@gmail.com>
2024-09-18 09:57:39 -0500

    add new endpoint to check-available fix

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import Utils from './utils.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import Utils from "./utils.js";
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.disable('x-powered-by')
+app.use(cors());
+app.disable("x-powered-by");
 
-app.get('/', (req, res) => {
-  res.send('<h1>Tracker</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Tracker</h1>");
+});
 
-app.get('/performance', async (req, res) => {
-  const paramsGet = req.query
-  const urlDestino = paramsGet.url
-  const urlPost = process.env.URL_POST_PERFORMANCE
+app.get("/performance", async (req, res) => {
+  const paramsGet = req.query;
+  const urlDestino = paramsGet.url;
+  const urlPost = process.env.URL_POST_PERFORMANCE;
 
   const paramsPost = {
     email: paramsGet.email,
-    date: paramsGet.date
-  }
+    date: paramsGet.date,
+  };
 
   try {
-    await Utils.axiosPost(urlPost, paramsPost)
-    res.redirect(urlDestino)
+    await Utils.axiosPost(urlPost, paramsPost);
+    res.redirect(urlDestino);
   } catch (error) {
-    console.error(error.message)
-    res.status(500).send('Error')
+    console.error(error.message);
+    res.status(500).send("Error");
   }
-})
+});
 
-app.get('/review', async (req, res) => {
-  const paramsGet = req.query
-  const urlDestino = paramsGet.url
-  const urlPost = process.env.URL_POST
+app.get("/review", async (req, res) => {
+  const paramsGet = req.query;
+  const urlDestino = paramsGet.url;
+  const urlPost = process.env.URL_POST;
 
   const paramsPost = {
     emailToTrack: paramsGet.email,
     base: paramsGet.base,
-    date: paramsGet.date
-  }
+    date: paramsGet.date,
+  };
 
   try {
-    res.redirect(urlDestino)
-    await Utils.axiosPost(urlPost, paramsPost)
+    res.redirect(urlDestino);
+    await Utils.axiosPost(urlPost, paramsPost);
   } catch (error) {
-    console.error(error.message)
-    res.status(500).send('Error')
+    console.error(error.message);
+    res.status(500).send("Error");
   }
-})
+});
 
 app.get("/check-availability", async (req, res) => {
   const now = new Date();
   const day = now.getDay();
   const hour = now.getHours();
 
-  let  isAvailable = false;
+  let isAvailable = false;
 
-  if(day >= 1 && day <= 5){
+  if (day >= 1 && day <= 5) {
     isAvailable = true;
-  }else if ((day === 0 || day === 6) && hour >= 10 && hour < 16) {
+  } else if ((day === 0 || day === 6) && hour >= 10 && hour < 16) {
     isAvailable = true;
   }
 
-  res.json({available: isAvailable})
-}
+  res.json({ available: isAvailable });
+});
 
 app.use((req, res) => {
-  res.send('<h1>404</h1>')
-})
+  res.send("<h1>404</h1>");
+});
 
 app.listen(PORT, () => {
-  console.log(`server listing on port ${PORT}`)
-})
+  console.log(`server listing on port ${PORT}`);
+});
